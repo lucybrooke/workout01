ddfc982961603cc3e1d4a079ddde58d1b34de4e3
Lucy Brooke <lucybrooke@berkeley.edu>
2019-03-13 22:38:44 -0700

    report finished

--- code/make-shot-charts-script.R
+++ code/make-shot-charts-script.R
@@ -31,8 +31,9 @@ ai_shot_chart <- ggplot(data = a_iguodala) +
   theme_minimal()
 ai_shot_chart
 
-# save ai_shot_chart as pdf
+# save ai_shot_chart as pdf and png
 ggsave("andre-iguodala-shot-chart.pdf", width = 6.5, height = 5)
+ggsave("andre-iguodala-shot-chart.png", width = 6.5, height = 5)
 
 # draymond green shot chart
 dg_shot_chart <- ggplot(data = d_green) +
@@ -43,8 +44,9 @@ dg_shot_chart <- ggplot(data = d_green) +
   theme_minimal()
 dg_shot_chart
 
-# save dg_shot_chart as pdf
+# save dg_shot_chart as pdf and png
 ggsave("draymond-green-shot-chart.pdf", width = 6.5, height = 5)
+ggsave("draymond-green-shot-chart.png", width = 6.5, height = 5)
 
 
 # kevin durant shot chart
@@ -56,8 +58,9 @@ kd_shot_chart <- ggplot(data = k_durant) +
   theme_minimal()
 kd_shot_chart
 
-# save kd_shot_chart as pdf
+# save kd_shot_chart as pdf and png
 ggsave("kevin-durant-shot-chart.pdf", width = 6.5, height = 5)
+ggsave("kevin-durant-shot-chart.png", width = 6.5, height = 5)
 
 # klay thompson shot chart
 kt_shot_chart <- ggplot(data = kl_thompson) +
@@ -68,11 +71,12 @@ kt_shot_chart <- ggplot(data = kl_thompson) +
   theme_minimal()
 kt_shot_chart
 
-# save kt_shot_chart as pdf
+# save kt_shot_chart as pdf and png
 ggsave("klay-thompson-shot-chart.pdf", width = 6.5, height = 5)
+ggsave("klay-thompson-shot-chart.png", width = 6.5, height = 5)
 
 
-# stephen curry shot chart
+# stephen curry shot chart and png
 sc_shot_chart <- ggplot(data = s_curry) +
   annotation_custom(court_image, -250, 250, -50, 420) +
   geom_point(aes(x = x, y = y, color = shot_made_flag), size = 1.5) +
@@ -81,8 +85,9 @@ sc_shot_chart <- ggplot(data = s_curry) +
   theme_minimal()
 sc_shot_chart
 
-# save sc_shot_chart as pdf
+# save sc_shot_chart as pdf and png
 ggsave("stephen-curry-shot-chart.pdf", width = 6.5, height = 5)
+ggsave("stephen-curry-shot-chart.png", width = 6.5, height = 5)
 
 # facetted shot chart
 
